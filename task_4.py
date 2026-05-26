60ed61371c9f3716d8b61bf3e1b6ff924abade76
babykasseta <qwerty1268100@gmail.com>
2026-05-26 18:12:22 +0700

    first commit

---
+++ task_4.py
@@ -0,0 +1,19 @@
+def function_one():
+    x = 1
+    y = 2
+    total = x + y
+    return total
+
+def function_two(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+class Test:
+    def method_one(self):
+        pass
+
+    def method_two(self):
+        pass
