f7f1eb8e4826304528eabfec172196d82a65710f
babykasseta <qwerty1268100@gmail.com>
2026-05-26 18:11:19 +0700

    first commit

---
+++ task_3.py
@@ -0,0 +1,6 @@
+def very_long_function(
+        param1, param2, param3,
+        param4, param5, param6
+):
+    total = 1.18 * (param1 + param2 + param3 + param4 + param5 + param6)
+    return total
