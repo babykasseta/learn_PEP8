98e259e922ef04d399bb7d466d613b99815dd1f3
babykasseta <qwerty1268100@gmail.com>
2026-05-26 18:16:55 +0700

    first commit

---
+++ task_6.py
@@ -0,0 +1,10 @@
+x = 1 + 2 * 3 - 4 / 5
+list_one = [1, 2, 3, 4, 5]
+dict_one = {'a': 1, 'b': 2, 'c': 3}
+
+def function(x = 0, y = 0):
+    return x ** 2 + y ** 2
+
+result = function(2, 3)
+if 10 < result < 20:
+    print("Result in range")
